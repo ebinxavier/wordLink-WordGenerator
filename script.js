47e856603fe3e883188347f31408700acbe38bd9
Ebin Xavier <ebinx7@gmail.com>
2019-09-03 18:08:21 +0400

    Removed duplicated results. Command Line support

--- script.js
+++ script.js
@@ -16,5 +16,13 @@ function getPermutations(n, r) {
     })([...t]);
     return result;
 }
-var WORD = "bowl", COUNT = 4;
-console.log(getPermutations(new Array(WORD.length).fill(0).map((e, i) => i), COUNT).map(el => el.reduce((a, w) => a += WORD[w], "")).filter(el => wordList.some(word => el == word)));
+var WORD = process.argv[2] || "bowl";
+var COUNT = Number(process.argv[3]) || WORD.length;
+if(COUNT>WORD.length){
+    console.log("Length should be less than word length !");   
+} else {
+let list = getPermutations(new Array(WORD.length).fill(0).map((e, i) => i), COUNT).map(el => el.reduce((a, w) => a += WORD[w], "")).filter(el => wordList.some(word => el == word));
+let set = new Set();
+list.forEach(e=>set.add(e));
+console.log(set);
+}
